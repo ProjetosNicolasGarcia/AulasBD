cff886068db49fb9438cbf90dfc120ff9a025535
Nicolas Garcia <100208523+ProjetosNicolasGarcia@users.noreply.github.com>
2022-05-06 09:01:27 -0300

    Add files via upload

---
+++ FLORICULTURA/FÍSICO_FLORICULTURA.sql
@@ -0,0 +1,39 @@
+-- Geração de Modelo físico
+-- Sql ANSI 2003 - brModelo.
+
+CREATE DATABASE FLORICULTURA;
+
+USE FLORICULTURA;
+
+CREATE TABLE CLIENTES (
+registro int PRIMARY KEY AUTO_INCREMENT,
+rg varchar(12),
+nome varchar(255),
+telefone varchar(14),
+endereco varchar(255)
+);
+
+CREATE TABLE COMPRAS (
+notafiscal int PRIMARY KEY AUTO_INCREMENT,
+valor_total decimal(10,2),
+registro int,
+FOREIGN KEY(registro) REFERENCES CLIENTES (registro)
+);
+
+CREATE TABLE compra-produto (
+data_compra date,
+qtde_itens int,
+codproduto int,
+notafiscal int,
+FOREIGN KEY(notafiscal) REFERENCES COMPRAS (notafiscal)
+);
+
+CREATE TABLE PRODUTOS (
+codproduto int PRIMARY KEY AUTO_INCREMENT,
+nomeproduto varchar(255),
+tipo varchar(255),
+preco decimal(10,2),
+qtde_estoque int
+);
+
+ALTER TABLE compra-produto ADD FOREIGN KEY(codproduto) REFERENCES PRODUTOS (codproduto)
